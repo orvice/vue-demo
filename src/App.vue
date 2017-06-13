101eae36189b9b762acb024be41bae462ef93b77
orvice <git@orx.me>
2017-06-14 02:05:46 +0800

    init now ui kit

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,31 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <Post></Post>
+    <div id="app">
+        <Nav></Nav>
+        <div  class="wrapper">
+            <div class="page-header clear-filter" filter-color="orange">
+                <div class="page-header-image" data-parallax="true" style="background-image: url('/assets/now/img/header.jpg');">
+                </div>
+                <div class="container">
+                    <div class="content-center">
+                        <h1 class="title text-center">Components Documentation</h1>
+                        <h3 class="category">v1.0.0</h3>
+                        <h4 class="description text-center">We are constantly doing updates for you. Please check the online documentation.</h4>
+                        <a href="http://demos.creative-tim.com/now-ui-kit/documentation/tutorial-components.html?ref=nuk-free-doc" class="btn btn-primary btn-lg btn-round">View Documentation</a>
+                    </div>
+                </div>
+            </div>
+            <Foot></Foot>
+        </div>
+    </div>
 </template>
-
 <script>
-
-import Post from './Post.vue'
-
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    import Nav from './components/Nav.vue'
+    import Foot from './components/Foot.vue'
+    export default {
+        name: 'app',
+        components: {
+            Nav,
+            Foot
+        }
     }
-  },
-  components: { Post }
-}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
